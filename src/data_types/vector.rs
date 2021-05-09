d6b8b77736b9343f43f6177ae7b28344575cb9a9
Or Gany <gangana3@gmail.com>
2021-05-09 22:00:02 +0300

    Adding div operator for Vector

--- src/data_types/vector.rs
+++ src/data_types/vector.rs
@@ -1,4 +1,5 @@
-use std::ops::{Add, Mul};
+use std::ops::{Add, Mul, Div};
+use std::intrinsics::powf32;
 
 pub struct Vector {
     x: f32,
@@ -9,6 +10,14 @@ pub struct Vector {
 impl Vector {
     pub fn new(x: f32, y: f32, z: f32) -> Vector {
         Vector {x, y, z}
+    }
+
+    pub fn length_squared(&self) -> f32 {
+        self.x.powf(2.0) + self.y.powf(2.0) + self.z.powf(2.0)
+    }
+
+    pub fn length(&self) -> f32 {
+        self.length_squared().sqrt()
     }
 }
 
@@ -47,3 +56,30 @@ impl Mul<f32> for Vector {
         )
     }
 }
+
+
+impl Div for Vector {
+    type Output = Vector;
+
+    fn div(self, rhs: Self) -> Self::Output {
+        Vector::new(
+            self.x / rhs.x,
+            self.y / rhs.y,
+            self.z / rhs.z
+        )
+    }
+}
+
+
+impl Div<f32> for Vector {
+    type Output = Vector;
+
+    fn div(self, rhs: f32) -> Self::Output {
+        Vector::new(
+            self.x / rhs,
+            self.y / rhs,
+            self.z / rhs
+        )
+    }
+}
+
